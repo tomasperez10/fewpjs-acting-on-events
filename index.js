d8aad3e5598b774fcd750783610a4de9d16f3369
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 09:40:31 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+let game = document.getElementById('game');
+
+function findLeftPosition() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  return left;
+}
+
+function moveDodgerLeft() {
+  let left = findLeftPosition();
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = findLeftPosition();
+
+  if (left >= 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+})
